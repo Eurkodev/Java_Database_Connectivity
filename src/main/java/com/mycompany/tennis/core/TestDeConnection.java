85a829900e769053f69700d9513dba1df25942ff
Eurkodev <rachedi.yakob@gmail.com>
2026-06-19 16:59:46 +0200

    Section_3_V12

--- src/main/java/com/mycompany/tennis/core/TestDeConnection.java
+++ src/main/java/com/mycompany/tennis/core/TestDeConnection.java
@@ -12,23 +12,22 @@ public class TestDeConnection {
             //MySQL driver MySQL Connector
 
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris&allowPublicKeyRetrieval=true","EURKODEV","Bouftou80@");
-            long identifiant = 128L;
-            PreparedStatement prepareStatement = conn.prepareStatement("SELECT ID, NOM, PRENOM FROM JOUEUR WHERE ID =? ");
-            prepareStatement.setLong(1, identifiant);
-            ResultSet rs = prepareStatement.executeQuery();
+            PreparedStatement prepareStatement = conn.prepareStatement("UPDATE JOUEUR SET NOM=?, PRENOM=? WHERE ID =? ");
+            long identifiant = 24L;
+            String nom = "Errani";
+            String prenom = "Sara";
+            prepareStatement.setString(1, nom);
+            prepareStatement.setString(2, prenom);
+            prepareStatement.setLong(3, identifiant);
+            int nbEnregistrementModifies = prepareStatement.executeUpdate();
 
-            while (rs.next()) {
-                final String nom=rs.getString("NOM");
-                final String prenom=rs.getString("PRENOM");
-                final Long id = rs.getLong("ID");
-                System.out.println("Le joueur / la joueuse représenté(e) par le numéro " + id + " est " + prenom + " " + nom );
-            }
-            prepareStatement.setLong(1,129);
-            ResultSet rs2=prepareStatement.executeQuery();
+
+
             //Oracle Driver officiel OJDBC Thin
             //conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:tennis","COURSDB","COURSDB");
             //Postgres Driver officiel
             //conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tennis","COURSDB","COURSDB");
+            System.out.println("Nombre d'enregistrements modifies : " + nbEnregistrementModifies);
             System.out.println("success");
         } catch (SQLException e) {
             e.printStackTrace();
