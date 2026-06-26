189307e85d66dc4096c2358833d283272d2996eb
Eurkodev <rachedi.yakob@gmail.com>
2026-06-26 16:39:05 +0200

    Section_4_V14_Classe_DataSource

--- src/main/java/com/mycompany/tennis/core/TestDeConnection.java
+++ src/main/java/com/mycompany/tennis/core/TestDeConnection.java
@@ -1,5 +1,7 @@
 package com.mycompany.tennis.core;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 import java.sql.*;
 
 public class TestDeConnection {
@@ -11,26 +13,50 @@ public class TestDeConnection {
 
             //MySQL driver MySQL Connector
 
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris&allowPublicKeyRetrieval=true","EURKODEV","Bouftou80@");
-            PreparedStatement prepareStatement = conn.prepareStatement("UPDATE JOUEUR SET NOM=?, PRENOM=? WHERE ID =? ");
-            long identifiant = 24L;
-            String nom = "Errani";
-            String prenom = "Sara";
+            MysqlDataSource dataSource = new MysqlDataSource();
+            //dataSource.setUrl("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris&allowPublicKeyRetrieval=true");
+            dataSource.setServerName("localhost");
+            dataSource.setPort(3306);
+            dataSource.setDatabaseName("tennis");
+            dataSource.setUser("EURKODEV");
+            dataSource.setPassword("Bouftou80@");
+            dataSource.setUseSSL(false);
+            dataSource.getAllowPublicKeyRetrieval();
+            dataSource.setServerTimezone("Europe/Paris");
+            conn = dataSource.getConnection();
+
+           // conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris&allowPublicKeyRetrieval=true","EURKODEV","Bouftou80@");
+
+            conn.setAutoCommit(false);
+            PreparedStatement prepareStatement = conn.prepareStatement("INSERT INTO JOUEUR (NOM, PRENOM, SEXE) VALUES (?, ?, ?)");
+            String nom = "Capriati";
+            String prenom = "Jennifer";
+            String sexe = "F";
             prepareStatement.setString(1, nom);
             prepareStatement.setString(2, prenom);
-            prepareStatement.setLong(3, identifiant);
+            prepareStatement.setString(3, sexe);
             int nbEnregistrementModifies = prepareStatement.executeUpdate();
 
+            conn.commit();
 
+            nom = "Johannson";
+            prenom = "Thomas";
+            sexe = "H";
 
-            //Oracle Driver officiel OJDBC Thin
-            //conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:tennis","COURSDB","COURSDB");
-            //Postgres Driver officiel
-            //conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tennis","COURSDB","COURSDB");
-            System.out.println("Nombre d'enregistrements modifies : " + nbEnregistrementModifies);
-            System.out.println("success");
+            prepareStatement.setString(1, nom);
+            prepareStatement.setString(2, prenom);
+            prepareStatement.setString(3, sexe);
+            prepareStatement.executeUpdate();
+
+            conn.commit();
+
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                if(conn!=null) conn.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
         finally {
             try {
