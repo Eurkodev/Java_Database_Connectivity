8fb0880267eb02ccbb39ed1aa5919c68a662d0b5
rache_ya <yakob.rachedi.auditeur@lecnam.net>
2026-07-28 13:13:54 +0200

    Section5_V22

--- src/main/java/com/mycompany/tennis/core/repository/MatchRepositoryImpl.java
+++ src/main/java/com/mycompany/tennis/core/repository/MatchRepositoryImpl.java
@@ -20,7 +20,7 @@ public class MatchRepositoryImpl {
 
             conn.setAutoCommit(false);
 
-            PreparedStatement prepareStatement = conn.prepareStatement("INSERT INTO MATCH_TENNIS (ID, ANNEE , TYPE_EPREUVE, ID_TOURNOI) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement prepareStatement = conn.prepareStatement("INSERT INTO MATCH_TENNIS (ID, TYPE_EPREUVE, ID_TOURNOI) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 
             prepareStatement.setLong(1, match.getEpreuve().getId());
             prepareStatement.setLong(2, match.getVainqueur().getId());
